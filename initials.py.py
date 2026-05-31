c55b6b6867be710d421137482574a7b34afddc75
Henry-huey <2425403643@live.gctu.edu.gh>
2026-05-31 20:35:10 +0000

    Add files via upload

---
+++ initials.py.py
@@ -0,0 +1,30 @@
+def main():
+#HTA
+#Print out my initials
+# Assignment Number 2
+#
+#Adjirakor Tettey Henry
+#2425403643
+#2425403643@live.gctu.edu.gh
+#Augustus
+#
+#On my honor,Adjirakor Tettey Henry,this programming assignment is my own work
+#and I have not provided this code to any other student.
+    print("...HTA")
+    print()
+
+    print("...HHH....HHH........TTTTTTTTTT............AA.........")
+    print("...HHH....HHH........TTTTTTTTTT...........AAAA........")
+    print("...HHH....HHH...........TTTT.............AA..AA.......")
+    print("...HHH....HHH...........TTTT............AAA..AAA......")
+    print("...HHHHHHHHHH...........TTTT...........AAAAAAAAAA.....")
+    print("...HHHHHHHHHH...........TTTT...........AAAAAAAAAA.....")
+    print("...HHH....HHH...........TTTT...........AAA....AAA.....")
+    print("...HHH....HHH...........TTTT...........AAA....AAA.....")
+    print("...HHH....HHH...**......TTTT......**...AAA....AAA...**")
+    print("...HHH....HHH...**......TTTT......**...AAA....AAA...**")
+    print()
+
+
+if __name__ == "__main__":
+    main()
